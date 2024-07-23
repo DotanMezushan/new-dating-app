47dc22a31532a92e779e1293b27c808785b77043
dotan mezushan <dotanbm3052@gmail.com>
2024-07-23 14:00:42 +0300

    creating routes path

--- dating-app/src/app/app.routes.ts
+++ dating-app/src/app/app.routes.ts
@@ -1,15 +1,37 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 export const routes: Routes = [
-    {
-        path: '',component : LoginComponent
-    },
     {
         path: 'login',component : LoginComponent
     },
     {
         path: 'signup',component : SignupComponent
-    }
+    },
+    {
+        path: 'members',component : MemberListComponent
+    },
+    {
+        path: 'members/:id',component : MemberDetailComponent
+    },
+    {
+        path: 'list',component : ListsComponent
+    },
+    {
+        path: 'messages',component : MessagesComponent
+    },
+    {
+        path: '',component : HomeComponent
+    },
+    {
+        path: '**',
+        component : HomeComponent,
+        pathMatch: 'full'
+    },
 ];
