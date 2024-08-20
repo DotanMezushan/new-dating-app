2d320765d5eea22e8744f75936b752f715f881f0
dotan mezushan <dotanbm3052@gmail.com>
2024-08-21 01:53:01 +0300

    file issue

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -4,10 +4,24 @@ namespace API.Controllers
 {
     public class FallbackController : Controller
     {
-        public ActionResult Index()
+        //public ActionResult Index()
+        //{
+        //    return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+        //        "wwwroot", "index.html"), "text/HTML");
+        //}
+
+        public IActionResult Index()
         {
-            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
-                "wwwroot", "index.html"), "text/HTML");
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                // Handle the file not found scenario, e.g., return a 404 page
+                throw new Exception(Directory.GetCurrentDirectory().ToString() + "File place issue ");
+                //return NotFound("");
+            }
+
+            return PhysicalFile(filePath, "text/html");
         }
     }
 }
