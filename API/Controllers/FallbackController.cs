4d3efde4332e9bd0a3347c15924745a86861be73
dotan mezushan <dotanbm3052@gmail.com>
2024-08-21 02:38:31 +0300

    FallbackController

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -4,24 +4,17 @@ namespace API.Controllers
 {
     public class FallbackController : Controller
     {
-        //public ActionResult Index()
-        //{
-        //    return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
-        //        "wwwroot", "index.html"), "text/HTML");
-        //}
-
         public IActionResult Index()
         {
             var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
 
             if (!System.IO.File.Exists(filePath))
             {
-                var directory = Directory.GetCurrentDirectory();
-        var files = Directory.GetFiles(Path.Combine(directory, "wwwroot"));
-        return NotFound($"File not found: {filePath}. Current directory: {directory}. Files in wwwroot: {string.Join(", ", files)}");
+                return NotFound($"File not found: {filePath}");
             }
 
             return PhysicalFile(filePath, "text/html");
         }
+
     }
 }
